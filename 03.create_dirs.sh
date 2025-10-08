b796a2dc2ce5be814494fa0d9028e65756d6476c
thomkmutt567 <thomkmutt@gmail.com>
2025-10-08 15:16:08 +0700

    Update 03.create_dirs.sh

--- 03.create_dirs.sh
+++ 03.create_dirs.sh
@@ -32,7 +32,7 @@ log_status() {
 # --- การอ่านและประมวลผลไฟล์ Input ---
 while IFS= read -r line; do
     # ลบอักขระ Carriage Return (CR หรือ ^M) ออกจากตัวแปร $line
-    line=$(echo "$line" | tr -d '\r') 
+    # line=$(echo "$line" | tr -d '\r') 
     # ส่วนแก้ไขทีหลังเอา ^M ออก
     # ข้ามบรรทัดที่ว่างเปล่าหรือ comment
     if [[ -z "$line" || "$line" =~ ^[[:space:]]*# ]]; then
